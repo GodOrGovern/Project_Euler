91ea6974b78d3aa0fe8727ca59421f6e43bef0be
David Keenan <willkeenan12@gmail.com>
2020-07-29 14:23:10 -0400

    Simplify xgcd()

--- problems/Python/e091.py
+++ problems/Python/e091.py
@@ -2,7 +2,7 @@
 and are joined to the origin to form ΔOPQ. Given that 0 ≤ x1, y1, x2,
 y2 ≤ 50, how many right triangles can be formed? '''
 
-from math import gcd
+#from math import gcd
 
 def main():
     ''' The number of triangles with the hypotenuse between points P and Q is
@@ -16,34 +16,24 @@ def main():
     solutions = set()
     for a in range(end+1):
         for b in range(1 if a == 0 else a, end+1):
-            c = a**2 + b**2
-            div = gcd(a, b)
-            if c % div != 0:
-                continue
-            x, y = (c//div*n for n in ext_euclid(a, b))
-            for n in range((x-end)*div//b, x*div//b + 1):
-                x1 = x - n * (b // div)
-                y1 = y + n * (a // div)
+            gcd, x, y = xgcd(a, b)
+            mult = (a**2 + b**2) // gcd
+            x, y = x*mult, y*mult
+            for n in range((x-end)*gcd//b, x*gcd//b + 1):
+                x1, y1 = x - n * (b // gcd), y + n * (a // gcd)
                 if 0 <= x1 <= end and 0 <= y1 <= end and (a, b) != (x1, y1):
                     solutions.add(tuple(sorted([(a, b), (x1, y1)])))
                     solutions.add(tuple(sorted([(b, a), (y1, x1)])))
     print(len(solutions) + end**2)
 
-def ext_euclid(a, b):
-    ''' Returns solutions for x and y in the equation ax + by = gcd(a, b) '''
-    q = 0
-    r0, r1, rNext = a, b, a
-    s0, s1, sNext = 1, 0, 1
-    t0, t1, tNext = 0, 1, 0
-    while r1 != 0:
-        q = r0 // r1
-        rNext = r0 - r1*q
-        sNext = s0 - s1*q
-        tNext = t0 - t1*q
-        r0, r1 = r1, rNext
-        s0, s1 = s1, sNext
-        t0, t1 = t1, tNext
-    return s0, t0
+def xgcd(a, b):
+    ''' Returns gcd(a, b) and solutions for x and y in the equation ax + by = gcd(a, b) '''
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        (q, a), b = divmod(b, a), a
+        x0, x1 = x1, x0 - x1*q
+        y0, y1 = y1, y0 - y1*q
+    return b, x0, y0
 
 if __name__ == "__main__":
     main()
