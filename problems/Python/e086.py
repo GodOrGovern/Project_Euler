8e7ee9625092ccbd86a8ffea76332608c48748d4
David Keenan <willkeenan12@gmail.com>
2020-07-29 19:25:06 -0400

    Solution to problem 86

--- problems/Python/e086.py
+++ problems/Python/e086.py
@@ -4,32 +4,25 @@ given cuboid and the shortest route doesn't always have integer length. Find
 the least value of M such that the number of solutions first exceeds one
 million '''
 
-def main():
-    ''' Driver function '''
-    sqrs = gen_sqrs(10000)
-    routes = []
-    max_val = 3305873
-    for n in range(1, 1300):
-        if 2 * sqrs[n] > max_val:
-            break
-        for m in range(n+1, n+1000):
-            route = sqrs[n] + sqrs[m]
-            if route > max_val:
-                break
-            routes += [sqrs[n] + sqrs[m]]
-    routes = sorted(routes)
-    print(routes[10**6])
-    print(routes[2060])
+from euler import gen_triple
 
-def gen_sqrs(end):
-    ''' Generate all squares up to 'end**2' '''
-    n = sqr = 1
-    sqrs = [sqr]
-    while n < end:
-        sqr += 2*n + 1
-        sqrs += [sqr]
-        n += 1
-    return sqrs
+def main():
+    ''' I manually binary searched by adjusting the 'end' variable '''
+    end = 1818
+    count = 0
+    for t in gen_triple():
+        if t.min() > end:
+            continue
+        t.sort()
+        a, b = t[0], t[1]
+        while a <= end and b <= 2*end:
+            for n in range(b-1 if b <= end else end, b//2-(b+1)%2, -1):
+                count += b-n <= n <= a
+            if b <= end:
+                for n in range(a-1, a//2-(a+1)%2, -1):
+                    count += a-n <= n <= b
+            a, b = a+t[0], b+t[1]
+        print(count)
 
 if __name__ == "__main__":
     main()
