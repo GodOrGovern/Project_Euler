3491b4a95c918de3d695a5d62b8540dab1b12527
David Keenan <willkeenan12@gmail.com>
2021-03-31 20:40:27 -0400

    Solution to problem 518

--- problems/Python/e518.py
+++ problems/Python/e518.py
@@ -2,41 +2,35 @@
 numbers; a < b < c < n; a+1, b+1, and c+1 form a geometric sequence. Find
 S(10**8) '''
 
-from collections import defaultdict
+from math import ceil, isqrt
+from pyprimesieve import factorize
 from pyprimesieve import primes as primes_to
 
 def main():
-    ''' Driver function '''
-    end = 10**4
-    print(progression(end))
+    ''' Takes about a minute to finish running '''
+    print(S(10**8))
 
-def progression(end):
-    ''' Build undirected graph where nodes are prime numbers and edges are
-    the ratio between the larger prime and the smaller prime (plus 1 in both
-    cases). Return the sum of nodes that are part of a geometric progression
-    '''
+def S(n):
+    ''' Implementation of the S(n) function described above '''
+    n_sqr = n*n
     total = 0
-    primes = primes_to(end)
-    graph = defaultdict(dict)
-    for i, p1 in enumerate(primes[:-1]):
-        if graph[p1].keys():
-            min_r = min(graph[p1].keys())
-            max_r = max(graph[p1].keys())
-        else:
-            min_r = 1
-            max_r = ((end-1) / (p1+1))**0.5
-        for p2 in primes[i+1:]:
-            r = (p2 + 1) / (p1 + 1)
-            if (p2+1)*r > end and max_r < r or min_r > r:
-                break
-            if r in graph[p1]:
-                total += p1 + p2 + graph[p1][r]
-            else:
-                graph[p1][r] = p2
-            if r in graph[p2]:
-                total += p1 + p2 + graph[p2][r]
-            else:
-                graph[p2][r] = p1
+    primes = primes_to(n)
+    primes_set = set(primes)
+    for i, a in enumerate(primes):
+        # m is an integer such that if f(x) = (x*m)**2 for any natural x, f(x)
+        # returns the xth perfect square divisible by a+1
+        # From: https://math.stackexchange.com/a/4084884/530956
+        m = 1
+        for p, e in factorize(a+1):
+            m *= p**(ceil(e/2))
+        m_sqr = m*m
+        x = a//n
+        val = m_sqr // (a+1)
+        c = x*x*val-1
+        while (c := c + val*(2*x+1)) < n:
+            x += 1
+            if (c in primes_set) and ((b := x*m-1) in primes_set) and (a < b < c):
+                total += a + b + c
     return total
 
 if __name__ == "__main__":
