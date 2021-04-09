b5a2bbfa1d7e71142bf8bd2e1aa36b3a5b9a1dcc
David Keenan <willkeenan12@gmail.com>
2021-04-09 12:46:41 -0400

    Solution to problem 204

--- problems/Python/e204.py
+++ problems/Python/e204.py
@@ -2,8 +2,29 @@
 it has no prime factor larger than n. How many generalised Hamming numbers of
 type 100 are there which don't exceed 10**9? '''
 
+from pyprimesieve import primes
+
 def main():
     ''' Driver function '''
+    print(len(general_hamming(10**9, 100)))
+
+def general_hamming(end, high):
+    ''' Return a set of all numbers (<= end) with prime factors not exceeding
+    high '''
+    hamming = {1}
+    prev = {1}
+    base = primes(high+1)
+    while 2*min(prev) <= end:
+        cur = set()
+        for n in prev:
+            for p in base:
+                temp = n*p
+                if temp > end:
+                    break
+                cur.add(temp)
+        hamming.update(cur)
+        prev = cur
+    return hamming
 
 if __name__ == "__main__":
     main()
