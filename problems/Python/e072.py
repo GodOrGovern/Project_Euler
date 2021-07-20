f82aab627fff9dabc912e71244d43876750ba1dd
David Keenan <willkeenan12@gmail.com>
2021-07-19 21:29:33 -0400

    Remove flint module and use local totient function

--- problems/Python/e072.py
+++ problems/Python/e072.py
@@ -1,12 +1,12 @@
 ''' How many elements would be contained in the set of reduced proper fractions
 for d ≤ 1,000,000? '''
 
-from flint import fmpz
+from euler import totients_to
 
 def main():
     ''' The sum of totient values up to n gives the number of reduced proper
     fractions with denominator less than or equal to n '''
-    print(sum(fmpz.euler_phi(fmpz(x)) for x in range(2, 10**6+1)))
+    print(sum(totients_to(10**6)[1:]))
 
 if __name__ == "__main__":
     main()
